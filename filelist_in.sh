2740fc906eeac510ab371716937cac021a646965
TheresaLang <theresa_lang@gmx.net>
2021-05-07 18:41:55 +0200

    implement dpp0029

---
+++ filelist_in.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+run=$1
+var=$2
+start_date=$3
+end_date=$4
+
+filelist=()
+declare -A var2file
+
+case ${run} in
+
+    "dpp0029")
+        directory="/mnt/lustre01/work/mh0287/k203123/GIT/icon-aes-dyw2/experiments/dpp0029"
+        var2file["T"]="dpp0029_atm_3d_1_ml_"
+        var2file["P"]="dpp0029_atm_3d_1_ml_"
+        var2file["U"]="dpp0029_atm_3d_2_ml_"
+        var2file["V"]="dpp0029_atm_3d_2_ml_"
+        var2file["OMEGA"]="dpp0029_atm_3d_3_ml_"
+        var2file["QV"]="dpp0029_atm_3d_4_ml_"
+        var2file["PS"]="dpp0029_atm_2d_ml_"
+
+        d=${start_date}
+        while [ ${d} != ${end_date} ]; do 
+            datestr=$(date -d "${d}" +%Y%m%d)
+            file="${directory}/${var2file[${var}]}${datestr}T000000Z.nc"
+            echo ${file}
+            #filelist=(${filelist[@]} ${file})
+            d=$(date -I -d "${d} + 1 day")
+        done
+        ;;
+    
+    *)
+        echo "Run ${run} unknown"
+        ;;
+esac
---
+++ filelist_out.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+out_dir=$1
+run=$2
+var=$3
+start_date=$4
+end_date=$5
+
+filelist=()
+
+case ${run} in
+
+    "dpp0029")
+        directory="${out_dir}/dpp0029"
+
+        d=${start_date}
+        while [ ${d} != ${end_date} ]; do 
+            datestr=$(date -d "${d}" +%Y%m%d)
+            file="${directory}/${var}_${datestr}_"
+            echo ${file}
+            d=$(date -I -d "${d} + 1 day")
+        done
+        ;;
+    
+    *)
+        echo "Run ${run} unknown"
+        ;;
+esac
