530149820309d473cac34b4f790b752b0f4c10ee
TheresaLang <theresa_lang@gmx.net>
2021-05-18 17:54:30 +0200

    Keep track of job IDs

--- process_icon_output.sh
+++ process_icon_output.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
+
 # general settings
 source config.sh
 
 # specific settings
-run="nwp0005"
-variables=("W")
-start_date="2020-02-01"
-end_date="2020-02-05"
-seltimestep="" # timesteps to select from each file; empty string --> select all timesteps
-lon_lat_box="-180,180,-70,70"
-grid_file="/work/mh0066/m300752/DYAMOND++/data/weight/griddes_1x1.txt"
-weights_file="/work/mh0066/m300752/DYAMOND++/data/weight/weight_dpp0016_1x1.nc"
-out_dir="/mnt/lustre02/work/mh1126/m300773/lagranto_input"
+run='nwp0005'
+variables=('W' 'PS')
+start_date='2020-02-01'
+end_date='2020-02-05'
+timesteps='00:00:00,03:00:00,06:00:00,09:00:00,12:00:00,15:00:00,18:00:00,21:00:00' 
+lon_lat_box='-180,180,-70,70'
+grid_file='/work/mh0066/m300752/DYAMOND++/data/weight/griddes_1x1.txt'
+weights_file='/work/mh0066/m300752/DYAMOND++/data/weight/weight_dpp0016_1x1.nc'
+out_dir='/mnt/lustre02/work/mh1126/m300773/lagranto_input'
 
+job_ids=""
 for var in ${variables[@]}; do
     echo ${var}
-    var_name_in=$(bash variable_name.sh ${run} ${var})
     read -r -a filelist_in <<< $(bash filelist_in.sh ${run} ${var} ${start_date} ${end_date})
     read -r -a filelist_out <<< $(bash filelist_out.sh ${out_dir} ${run} ${var} ${start_date} ${end_date})
     echo ${filelist_in[1]}
     for i in ${!filelist_in[@]}; do
-        sbatch process_file.sh ${filelist_in[i]} ${filelist_out[i]} ${grid_file} ${weights_file} ${var} ${lon_lat_box} ${seltimestep}
+        id=$(sbatch process_file.sh ${filelist_in[i]} ${filelist_out[i]} ${grid_file} ${weights_file} ${var} ${lon_lat_box} ${timesteps})
+	job_ids="${job_ids}:${id:20:28}"
     done
-done    
+done
+
+# merge files
+#sbatch "--dependency=afterok${job_ids}" merge_files.sh 
 
 # wenn alle jobs durchgelaufen sind: merge jeweils alle files (=Variablen), die zu einem Zeitschritt gehören
 # OMEGA bze. W muss vertical interpoliert werden
