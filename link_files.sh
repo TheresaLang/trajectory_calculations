52d8fede7f4b37a1ec7755e1a70f1da69629b6b8
TheresaLang <theresa_lang@gmx.net>
2021-05-21 13:39:00 +0200

    Script to link ICON files in lagranto run directory

---
+++ link_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source_dir="/mnt/lustre02/work/mh1126/m300773/lagranto_input/nwp0005"
+target_dir="/mnt/lustre02/work/um0878/users/tlang/work/rh_sensitivity/lagranto/runscripts/first_test"
+
+start_date='2020-02-01 03:00'
+end_date='2020-02-05 00:00'
+timestep=180 #in minutes
+
+d=${start_date}
+while [ "${d}" != "${end_date}" ]; do
+    datestr=$(date -d "${d}" +%Y%m%d_%H)
+    ln -s "${source_dir}/P${datestr}" "${target_dir}"
+    d=$(date -d "${d} ${timestep} minutes" "+%Y-%m-%d %H:%M")
+done
+
+
+
+
+
