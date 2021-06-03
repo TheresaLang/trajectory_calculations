727f8932039a62f209f942dee54157d9217830cd
TheresaLang <theresa_lang@gmx.net>
2021-06-03 17:18:56 +0200

    Implement ots0001

--- filelist_out.sh
+++ filelist_out.sh
@@ -7,12 +7,10 @@ start_date=$4
 end_date=$5
 
 filelist=()
-
+directory="${out_dir}/${run}"
 case ${run} in
 
     "dpp0029")
-        directory="${out_dir}/dpp0029"
-
         d=${start_date}
         while [ "${d}" != "${end_date}" ]; do 
             datestr=$(date -d "${d}" +%Y%m%d)
@@ -23,14 +21,26 @@ case ${run} in
         ;;
     
     "nwp0005")
-        directory="${out_dir}/nwp0005"
-
         d=${start_date}
         while [ "${d}" != "${end_date}" ]; do 
             datestr=$(date -d "${d}" +%Y%m%d)
             file="${directory}/${var}_${datestr}_"
             echo ${file}
             d=$(date -d "${d} + 1 day" "+%Y-%m-%d %H:%M")
+        done
+        ;;
+
+    "ots0001")
+        d=${start_date}
+        while [ "${d}" != "${end_date}" ]; do 
+            datestr=$(date -d "${d}" +%Y%m%d_%H%M)
+            file="${directory}/${var}_${datestr}_"
+            echo ${file}
+            if [[ ${var} == 'PS' || ${var} == 'PW' ]]; then
+                d=$(date -d "${d} + 1 day" "+%Y-%m-%d %H:%M")
+            else
+                d=$(date -d "${d} 3 hours" "+%Y-%m-%d %H:%M")
+            fi
         done
         ;;
     *)
