d797902645e9a10f45b23a0891f72b76b8698ff6
TheresaLang <theresa_lang@gmx.net>
2021-06-04 15:12:00 +0200

    Move config part to config.sh

--- link_files.sh
+++ link_files.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-source_dir="/mnt/lustre02/work/mh1126/m300773/lagranto_input/nwp0005"
-target_dir="/mnt/lustre02/work/um0878/users/tlang/work/rh_sensitivity/lagranto/runscripts/first_test"
-
-start_date='2020-02-01 03:00'
-end_date='2020-02-05 00:00'
-timestep=180 #in minutes
+source config.sh
 
 d=${start_date}
 while [ "${d}" != "${end_date}" ]; do
