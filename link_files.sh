eddefbaf311bfe9d0def7784f9647a6770783ba9
TheresaLang <theresa_lang@gmx.net>
2021-06-04 15:17:10 +0200

    Make variable names consistent with process_icon_output.sh

--- link_files.sh
+++ link_files.sh
@@ -4,8 +4,8 @@ source config.sh
 d=${start_date}
 while [ "${d}" != "${end_date}" ]; do
     datestr=$(date -d "${d}" +%Y%m%d_%H%M)
-    ln -s "${source_dir}/P${datestr}" "${target_dir}"
-    ln -s "${source_dir}/S${datestr}" "${target_dir}"
+    ln -s "${out_dir}/${run}/P${datestr}" "${run_dir}"
+    ln -s "${out_dir}/${run}/S${datestr}" "${run_dir}"
     d=$(date -d "${d} ${timestep} minutes" "+%Y-%m-%d %H:%M")
 done
 
