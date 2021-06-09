0377b8c3e97f8c53c53d8b2c8af6ce0437180e57
TheresaLang <theresa_lang@gmx.net>
2021-06-09 11:42:47 +0200

    Modify naming of linked files
    Only add minutes (%M) if the timestep is below one hour.

--- link_files.sh
+++ link_files.sh
@@ -4,8 +4,13 @@ source config.sh
 d=${start_date}
 while [ "${d}" != "${end_date}" ]; do
     datestr=$(date -d "${d}" +%Y%m%d_%H%M)
-    ln -s "${out_dir}/${run}/P${datestr}" "${run_dir}"
-    ln -s "${out_dir}/${run}/S${datestr}" "${run_dir}"
+    if [[ ${timestep} -lt 60 ]]; then
+    	datestr_out=$(date -d "${d}" +%Y%m%d_%H%M)
+    else
+    	datestr_out=$(date -d "${d}" +%Y%m%d_%H)
+    fi
+    ln -s "${out_dir}/${run}/P${datestr}" "${run_dir}/P${datestr_out}"
+    ln -s "${out_dir}/${run}/S${datestr}" "${run_dir}/S${datestr_out}"
     d=$(date -d "${d} ${timestep} minutes" "+%Y-%m-%d %H:%M")
 done
 
