12a35d8af0c4e0ba0c67b47ba396b7f2598e5536
TheresaLang <theresa_lang@gmx.net>
2021-06-18 18:56:38 +0200

    Adjust to DYAMOND_S_5km

--- create_iconconst.sh
+++ create_iconconst.sh
@@ -10,11 +10,14 @@ topo_file="${out_dir}/${run}/topo.nc"
 full_levels_file="${out_dir}/${run}/model_full_levels.nc"
 half_levels_file="${out_dir}/${run}/model_half_levels.nc"
 out_file="${out_dir}/${run}/ICONCONST"
+half_levels_name="HHL"
+topo_name="HSURF"
 
-cdo ${CDO_OPTS} sellonlatbox,${lon_lat_box} -remap,${grid_file},${weights_file} ${height_file} ${temp_file}
-cdo ${CDO_OPTS} --reduce_dim sellevidx,$half_level_end -chname,zghalf,TOPOGRAPHY -selvar,zghalf ${temp_file} ${topo_file}
-#cdo ${CDO_OPTS} sellevidx,$level_start/$level_end -chname,zghalf,z_mc,height_2,height -selvar,zghalf ${temp_file} ${full_levels_file}
-cdo ${CDO_OPTS} sellevidx,$half_level_start/$half_level_end -chname,zghalf,z_mc,height,height_2 -selvar,zghalf ${temp_file} ${half_levels_file}
+cdo ${CDO_OPTS} -f nc4 sellonlatbox,${lon_lat_box} -remap,${grid_file},${weights_file} ${height_file} ${temp_file}
+#cdo ${CDO_OPTS} --reduce_dim sellevidx,$half_level_end -chname,${topo_name},TOPOGRAPHY -selvar,${topo_name} ${temp_file} ${topo_file}
+cdo ${CDO_OPTS} -f --reduce_dim nc4 chname,${topo_name},TOPOGRAPHY -selvar,${topo_name} ${temp_file} ${topo_file}
+#cdo ${CDO_OPTS} sellevidx,$level_start/$level_end -chname,${half_levels_name},z_mc,height_2,height -selvar,${half_levels_name} ${temp_file} ${full_levels_file}
+cdo ${CDO_OPTS} -f nc4 sellevidx,$half_level_start/$half_level_end -chname,${half_levels_name},z_mc -selvar,${half_levels_name} ${temp_file} ${half_levels_file}
 
 cdo merge ${topo_file} ${half_levels_file} ${out_file}
 
