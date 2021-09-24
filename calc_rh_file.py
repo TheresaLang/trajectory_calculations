93f82fb168d6dab31b0635f41cf5f60c76738437
TheresaLang <theresa_lang@gmx.net>
2021-09-24 18:55:01 +0200

    Calculate FTH instead of CRH
    FTH: Average of RH over a given altitude region
    CRH: IWV/IWV_sat in a given pressure region

--- calc_rh_file.py
+++ calc_rh_file.py
@@ -17,22 +17,29 @@ import xarray as xr
 import sys
 import numpy as np
 import typhon
+import trajectory_utils
 
 P_file = sys.argv[1]
 S_file = sys.argv[2]
 z_file = sys.argv[3]
 RH_file = sys.argv[4]
 
-def calc_crh(vmr, p, t, z, p_start=85000, p_end=30000):
-    """ Calculate column relative humidity"""
-    in_p_range = np.logical_and(p <= p_start, p >= p_end)
-    vmr[np.logical_not(in_p_range)] = 0.
-    vmr_s = typhon.physics.e_eq_mixed_mk(t) / p
-    vmr_s[np.logical_not(in_p_range)] = 0.
-    iwv = typhon.physics.integrate_water_vapor(vmr, p, t, z)
-    iwv_s = typhon.physics.integrate_water_vapor(vmr_s, p, t, z)
-    crh = iwv / iwv_s
-    return crh
+def calc_fth(rh, z, layer_thickness, z_start=4000, z_end=8000):
+    """ Calculate mean relative humidity over a given height level"""
+    
+    fth = np.empty_like(rh[0])
+    fth[:] = np.nan
+    
+    for la in range(len(lat)):
+        for lo in range(len(lon)):
+            ind_fth = np.logical_and(z[:, la, lo] > z_start, z[:, la, lo] < z_end)
+            fth[la, lo] = np.average(rh[ind_fth, la, lo], weights=layer_thickness[ind_fth, la, lo], axis=0)
+    
+    # set FTH to nan, where mountains protrude into the FTH layer
+    fth[z[-1] > z_start] = np.nan
+        
+    return fth
+    
 
 # read dimensions, p, T and q from files
 ds_P = xr.load_dataset(P_file)
@@ -40,28 +47,27 @@ ds_S = xr.load_dataset(S_file)
 ds_z = xr.load_dataset(z_file)
 
 time = ds_P["time"]
-height = ds_P["height_2"]
 lon = ds_P["lon"]
 lat = ds_P["lat"]
-p = ds_P["P"]
-t = ds_S['T']
-q = ds_S['QV']
-z_half = ds_z['z_mc']
-z = z_half[:-1] + 0.5 * np.diff(z_half, axis=0)
+p = ds_P["P"].data[0]
+t = ds_S['T'].data[0]
+q = ds_S['QV'].data[0] * 1e-6
+z_half = ds_z['z_mc'].data
+layer_thickness = np.diff(z_half, axis=0)
+z = z_half[:-1] + 0.5 * layer_thickness
 
-# calculate RH and CRH
-vmr = typhon.physics.specific_humidity2vmr(q.data) #volume mixing ratio (vmr) 
-rh = typhon.physics.vmr2relative_humidity(vmr, p.data, t.data, e_eq=typhon.physics.e_eq_mixed_mk) #RH
-crh = calc_crh(vmr[0], p.data[0], t.data[0], z.data) #CRH
+# calculate RH and FTH
+rh = trajectory_utils.relative_humidity(q, t, p) #RH
+fth = calc_fth(rh, z, layer_thickness) #FTH
 
 # save
+rh = np.expand_dims(rh, 0)
 ds_RH = xr.Dataset(
     {
-        "RH": (p.dims, rh),
-        "CRH": (["lat", "lon"], crh)
+        "RH": (ds_S["T"].dims, rh),
+        "FTH": (["lat", "lon"], fth)
     },
-    coords = ds_P.coords
+    coords = ds_S.coords
 )
 ds_RH.to_netcdf(RH_file)
-
 
