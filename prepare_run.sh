d94ca07898778f656a1f61552e43625670c6b602
TheresaLang <theresa_lang@gmx.net>
2021-09-28 16:27:34 +0200

    Pass input_data_dir as argument to startf_from_rh.py

--- prepare_run.sh
+++ prepare_run.sh
@@ -13,7 +13,8 @@ bash link_runscripts.sh
 # create startf files
 echo 'create startf files' 
 num_batches=$((${num_nodes_per_start_time}*${num_cores_per_node}))
-if [[ region_def == 'pw' ]]; then
+if [[ ${region_def} == 'pw' ]]; then
     python ../random_starting_points/startf_from_pw.py ${exp_name} ${lagranto_run_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
-elif [[ region_def == 'rh' ]]; then
-    python ../random_starting_points/startf_from_rh.py ${exp_name} ${lagranto_run_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end} 
+elif [[ ${region_def} == 'rh' ]]; then
+    python ../random_starting_points/startf_from_rh.py ${exp_name} ${lagranto_run_dir} ${input_data_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
+fi 
