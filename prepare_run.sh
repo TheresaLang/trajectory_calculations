a14a7157c81aa70566f16f3ad44c34333b8510c3
TheresaLang <theresa_lang@gmx.net>
2021-10-05 09:57:35 +0200

    Make startf_from_pw.py and startf_from_rh.py batch scripts

--- prepare_run.sh
+++ prepare_run.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 set -o errexit -o nounset
 source run_config
+slurm_options="--partition=${partition} --cpus-per-task=${cpus_per_task} --time=1:00:00"
 # create run directory
 [ ! -d ${lagranto_run_dir} ] && mkdir ${lagranto_run_dir}
 [ ! -d ${lagranto_run_dir}/logs ] && mkdir ${lagranto_run_dir}/logs
@@ -14,7 +15,7 @@ bash link_runscripts.sh
 echo 'create startf files' 
 num_batches=$((${num_nodes_per_start_time}*${num_cores_per_node}))
 if [[ ${region_def} == 'pw' ]]; then
-    python ../random_starting_points/startf_from_pw.py ${exp_name} ${lagranto_run_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
+    sbatch ${slurm_options} ../random_starting_points/startf_from_pw.py ${exp_name} ${lagranto_run_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
 elif [[ ${region_def} == 'rh' ]]; then
-    python ../random_starting_points/startf_from_rh.py ${exp_name} ${lagranto_run_dir} ${input_data_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
+    sbatch ${slurm_options} ../random_starting_points/startf_from_rh.py ${exp_name} ${lagranto_run_dir} ${input_data_dir} "${first_start_time}" "${last_start_time}" ${start_time_interval} ${num_traj_per_start_time} ${num_batches} ${val_start} ${val_end} ${height_start} ${height_end}
 fi 
