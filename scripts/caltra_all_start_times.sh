fd7a3c4eabfa4598fcc21ce3664b4f63bd0bd3d4
TheresaLang <theresa_lang@gmx.net>
2021-10-27 11:48:24 +0200

    Remove unnecessary indent

--- scripts/caltra_all_start_times.sh
+++ scripts/caltra_all_start_times.sh
@@ -3,10 +3,10 @@
 source run_config
 
 d_start=${first_start_time}
-    while [ "${d_start}" != "${last_start_time}" ]; do
-        echo ${d_start}
-        d_end=$(date -d "${d_start} ${trajectory_length} hours ago" "+%Y-%m-%d %H:%M")
-        bash ./caltra_one_start_time.sh "${d_start}" "${d_end}"
-        d_start=$(date -d "${d_start} ${start_time_interval} hours" "+%Y-%m-%d %H:%M")
-    done
+while [ "${d_start}" != "${last_start_time}" ]; do
+    echo ${d_start}
+    d_end=$(date -d "${d_start} ${trajectory_length} hours ago" "+%Y-%m-%d %H:%M")
+    bash ./caltra_one_start_time.sh "${d_start}" "${d_end}"
+    d_start=$(date -d "${d_start} ${start_time_interval} hours" "+%Y-%m-%d %H:%M")
+done
 
