921457ddccf13676b5e61c6edfc09d681b6d6eef
TheresaLang <theresa_lang@gmx.net>
2021-10-27 10:25:49 +0200

    Initial commit

---
+++ scripts/trace_all_start_times.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source run_config
+
+d_start=${first_start_time}
+while [ "${d_start}" != "${last_start_time}" ]; do
+    bash ./caltra_one_start_time.sh ${d_start}
+    d_start=$(date -d "${d_start} ${start_time_interval} hours" "+%Y-%m-%d %H:%M")
+done
+
