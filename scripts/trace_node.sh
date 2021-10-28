cda83d54dba4b8771d078e828eb5f89bce8cba6c
TheresaLang <theresa_lang@gmx.net>
2021-10-28 11:16:53 +0200

    Change slurm options
    * Use whole node (exlusive mode)
    * Use all memory on node

--- scripts/trace_node.sh
+++ scripts/trace_node.sh
@@ -4,7 +4,8 @@
 
 #SBATCH --account=mh1126             # Charge resources on this project account
 #SBATCH --ntasks=1                   # max. number of tasks to be invoked
-##SBATCH --mem=0                      # use all memory on node
+#SBATCH --mem=0                      # use all memory on node
+#SBATCH --exclusive                  # use whole node
 ##SBATCH --mail-type=FAIL            # Notify user by email in case of job failure
 set -o errexit -o nounset
 source /mnt/lustre02/work/um0878/users/tlang/dev/lagranto/lagranto_config
