d686ccd85ce7822f977ddbcbef4902ad35eef758
TheresaLang <theresa_lang@gmx.net>
2021-10-28 11:20:55 +0200

    Change slurm settings
    * Decrease runtime
    * Do not use fat memory nodes exclusively

--- process_file.sh
+++ process_file.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=compute,compute2 # partition name
 #SBATCH --ntasks=1                   # max. number of tasks to be invoked
 #SBATCH --cpus-per-task=36           # number of CPUs per task
-#SBATCH --time=01:00:00              # limit on the total run time
+#SBATCH --time=00:30:00              # limit on the total run time
 #SBATCH --mem=0                      # use all memory on node
 ##SBATCH --constraint=256G           # only run on fat memory nodes 
 ##SBATCH --mail-type=FAIL            # Notify user by email in case of job failure
