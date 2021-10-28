06ccfbff76c1144b97d5d252dfb52c6d4cdcd36d
TheresaLang <theresa_lang@gmx.net>
2021-10-28 10:29:27 +0200

    Fix bug (missing quotation marks)

--- scripts/trace_all_start_times.sh
+++ scripts/trace_all_start_times.sh
@@ -4,7 +4,7 @@ source run_config
 
 d_start=${first_start_time}
 while [ "${d_start}" != "${last_start_time}" ]; do
-    bash ./caltra_one_start_time.sh ${d_start}
+    bash ./trace_one_start_time.sh "${d_start}"
     d_start=$(date -d "${d_start} ${start_time_interval} hours" "+%Y-%m-%d %H:%M")
 done
 
