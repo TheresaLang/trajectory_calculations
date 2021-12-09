ab5bc561ee3f484681d4103d994f15ba46babbf9
TheresaLang <theresa_lang@gmx.net>
2021-12-09 12:20:39 +0100

    Add test run for L330 (Can probably be removed later)

--- icon_output_postprocessing/datelist_out.sh
+++ icon_output_postprocessing/datelist_out.sh
@@ -15,6 +15,15 @@ case ${run} in
             d=$(date -d "${d} 1 day" "+%Y-%m-%d %H:%M")
         done
         ;;
+    
+    "atm_dyamond_summer_avr03_05s")
+        d=${start_date}
+        while [ "${d}" != "${end_date}" ]; do 
+            datestr=$(date -d "${d}" "+%Y%m%d %H%M")
+            echo "${datestr},"
+            d=$(date -d "${d} 3 hours" "+%Y-%m-%d %H:%M")
+        done
+    ;;
    
     "hsc0036") 
         d=${start_date}
