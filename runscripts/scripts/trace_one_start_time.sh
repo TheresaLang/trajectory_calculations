dde4c867fc393b42e24ffd79edd9e8086cd217ae
TheresaLang <theresa_lang@gmx.net>
2022-02-01 10:00:02 +0100

    Change squeue command to get status of jobs

--- runscripts/scripts/trace_one_start_time.sh
+++ runscripts/scripts/trace_one_start_time.sh
@@ -9,7 +9,7 @@ sbatch ${slurm_options} trace_node.sh "${start_time}"
 
 stat=$(squeue --start -u $USER | wc -l)
 while ((${stat} > 1)); do
-    stat=$(squeue --start -u $USER | wc -l)
+    stat=$(squeue --start -n trace_node.sh | wc -l)
     echo "Wait until job is running"
     sleep 5
 done
