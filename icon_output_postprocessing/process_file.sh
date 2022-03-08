cfa8a9910cf8d287ae96f4ee787dcf066107041c
Theresa Lang <theresa.lang@mpimet.mpg.de>
2022-03-08 16:23:58 +0100

    Adapt slurm options for Levante

--- icon_output_postprocessing/process_file.sh
+++ icon_output_postprocessing/process_file.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=process_file
+#SBATCH --job-name=postprocessing
 #SBATCH --output=logs/process_file-%j.out
 ##SBATCH --error=logs/process_file-%j.err
 
-#SBATCH --account=mh1126             # Charge resources on this project account
-#SBATCH --partition=compute,compute2 # partition name
+#SBATCH --account=mh0287             # Charge resources on this project account
+#SBATCH --partition=compute          # partition name
 #SBATCH --ntasks=1                   # max. number of tasks to be invoked
 #SBATCH --cpus-per-task=36           # number of CPUs per task
-#SBATCH --time=00:30:00              # limit on the total run time
+#SBATCH --time=04:00:00              # limit on the total run time
 #SBATCH --mem=0                      # use all memory on node
 ##SBATCH --constraint=256G           # only run on fat memory nodes 
 ##SBATCH --mail-type=FAIL            # Notify user by email in case of job failure
