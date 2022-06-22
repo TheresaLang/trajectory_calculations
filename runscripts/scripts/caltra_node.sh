d707ab450621f900798257c902c775108d4a91de
TheresaLang <theresa_lang@gmx.net>
2022-06-22 10:36:05 +0200

    Execute caltra in the foreground

--- runscripts/scripts/caltra_node.sh
+++ runscripts/scripts/caltra_node.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=caltra
 #SBATCH --output=logs/traj-%j.out
 #SBATCH --ntasks=1                   # max. number of tasks to be invoked
-#SBATCH --mem=0                      # use all memory on node
+##SBATCH --mem=0                      # use all memory on node
 set -o errexit -o nounset
 
 source /work/mh1126/m300773/dev/lagranto/lagranto_config
@@ -13,6 +13,4 @@ end_time=$2
 
 start_time_str=$(date -d "${start_time}" "+%Y%m%d_%H%M")
 end_time_str=$(date -d "${end_time}" "+%Y%m%d_%H%M")
-caltra.icon ${start_time_str} ${end_time_str} "${exp_name}_startf_${start_time_str}" "${exp_name}_trajectory_${start_time_str}.4" -j -p > "logs/${exp_name}_caltra_${start_time_str}.out" & 
-
-wait
+caltra.icon ${start_time_str} ${end_time_str} "${exp_name}_startf_${start_time_str}" "${exp_name}_trajectory_${start_time_str}.4" -j -p > "logs/${exp_name}_caltra_${start_time_str}.out"
